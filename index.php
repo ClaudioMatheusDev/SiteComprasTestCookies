4ac1f89b27e0a643d30acda91737592195b98805
Claudio Matheus <claudiomatheus055@gmail.com>
2024-08-02 08:27:35 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de compra</title>
+
+    <!-- BOOTSTRAP SECTION -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- BOOTSTRAP SECTION -->
+
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<header>  
+    <div class="nav">
+        <a href="#home">Início</a>
+        <a href="#produtos">Produtos</a>
+        <a href="#carrinho">Carrinho</a>
+        <a href="#sobre">Sobre</a>
+    </div>
+</header>
+
+<!-- HOME SECTION -->
+<section class="produtos" id="produtos"> 
+ <div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="https://images.tcdn.com.br/img/img_prod/769517/tenis_casual_nike_air_max_sc_20627_1_496584ab898e3e4d5c764c53b0a54f6a.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<a href="#" class="btn">Adicionar ao carrinho</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://static.prospin.com.br/media/catalog/product/i/d/id2471-tenis-adidas-game-spec-2-unis-preto-vermelho-e-branco.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<a href="#" class="btn">Adicionar ao carrinho</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://images.tcdn.com.br/img/img_prod/585092/tenis_adidas_ultra_bounce_feminino_9563_1_9908dcb5ab3471c973d944ba1683ac21.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+<a href="#" class="btn">Adicionar ao carrinho</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://static.prospin.com.br/media/catalog/product/f/b/fb7065-600-tenis-nike-zoom-vapor-lite-2-hc-prm-fem.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<a href="#" class="btn">Adicionar ao carrinho</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="https://artwalk.vtexassets.com/arquivos/ids/440784/DM003-2-006-1.jpg?v=638296160140430000" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<a href="#" class="btn">Adicionar ao carrinho</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://cdn.dooca.store/100/products/tenis-nike-revolution-7-masculino-wolf-grey.jpg?v=1706814592" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<a href="#" class="btn">Adicionar ao carrinho</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://cdn.shoppub.io/cdn-cgi/image/w=1000,h=1000,q=80,f=auto/spacetennis/media/uploads/produtos/foto/snpxlfti/mirai-51.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+	<a href="#" class="btn">Adicionar ao carrinho</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCoevOt3aKerX8jl23g_O6y8gQEuTYkkHHA&s" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	<a href="#" class="btn">Adicionar ao carrinho</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+</section>
+<!-- HOME SECTION -->
+
+</body>
+</html>
